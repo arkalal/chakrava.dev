6d0418bab8b25eeb437e0e3efd7f00e397f33d7b
arkalal <arkalal.123@gmail.com>
2024-06-05 19:56:34 +0530

    testing done

--- src/app/page.js
+++ src/app/page.js
@@ -1,23 +1,10 @@
 import React from "react";
-import Button from "../../components/library/src/Button/Button";
+import Home from "../../components/Home/Home";
 
 const page = () => {
   return (
     <div>
-      <Button
-        padding="10px 30px"
-        margin="10px"
-        bgColor="green"
-        textColor="yellow"
-        border="2px solid red"
-        borderRadius="10px"
-        width="150px"
-        height="50px"
-        hoverBgColor="darkgreen"
-        hoverTextColor="black"
-      >
-        Custom Button
-      </Button>
+      <Home />
     </div>
   );
 };
