bb3ea18c4eb40e083bbf0bbf5bf85d727d172326
arkalal <arkalal.123@gmail.com>
2024-08-05 13:37:34 +0530

    fixing issues

--- src/app/api/prices/route.js
+++ src/app/api/prices/route.js
@@ -5,13 +5,13 @@ const stripe = new Stripe(
   "sk_test_51PidbBSFWUyFMrsbtKbxsmr1QOF0QE8xUJmTmwwZcUXRsQ1vDnxJGM4a4RzBHsS9JP23n4DFUMM7S1XAT5kXJCYt00o9OYLxcA"
 );
 
-export async function GET() {
+export async function GET(req) {
   try {
     console.log("Fetching Stripe prices...");
     console.log("Stripe API Key:", process.env.STRIPE_API_KEY);
 
     const prices = await stripe.prices.list({
-      limit: 10, // Adjust the limit based on your needs
+      limit: 2, // Adjust the limit based on your needs
     });
 
     console.log("Prices fetched successfully:", prices);
